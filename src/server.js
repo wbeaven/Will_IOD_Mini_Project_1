46d4b28fb71e89d525e163450d384b5191eb76c6
Will <wtbeaven@gmail.com>
2025-04-28 18:27:12 +1000

    Cleaned up directory

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/", express.static("./_updated_version/public"));
+app.use("/", express.static("./src/public"));
 
 app.get("/days", (req, res) => {
   res.json(days);
